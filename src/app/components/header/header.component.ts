3f8a7367e0898db5168185d45c8c3620c03d95d1
adityajaiswal37 <adijaiswal0307@gmail.com>
2025-02-07 12:42:55 +0530

    fix

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+  isMenuOpen = false;
+    
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
